ff6e4856ca7b383e6a3c1ebe3cb7466755d72436
Luiz Moura <moura.oliveira.luiz@gmail.com>
2022-04-19 23:07:23 -0400

    Create levels

---
+++ app/Http/Controllers/LevelController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\LevelResource;
+use App\Models\Level;
+use Illuminate\Http\Request;
+
+class LevelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $levels = Level::paginate(20);
+
+        return $levels;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $level = Level::create($request->only(['name', 'description']));
+
+        return new LevelResource($level);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Level  $level
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Level $level)
+    {
+        return new LevelResource($level);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Level  $level
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Level $level)
+    {
+        $level->update($request->only(['name', 'description']));
+
+        return new LevelResource($level);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Level  $level
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Level $level)
+    {
+        $level->delete();
+
+        return response()->json(null, 204);
+    }
+}
